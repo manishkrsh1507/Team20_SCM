b88aa0b12ab23b859500c8aa351c9428b0ad4c21
Manish Kumar <manishkrsh1507@gmail.com>
2024-12-02 15:17:06 +0530

    Initial commit of Xcode project

---
+++ Team20_SCM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team20_SCM
+//
+//  Created by Manish Sahani on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
